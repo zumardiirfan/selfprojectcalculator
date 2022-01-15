c4e0318c75d8e5ed293ce1b0854e28f36e94f766
zumardiirfan <zumardiirfan@gmail.com>
2022-01-15 14:14:23 +0700

    Code 1 dari awal sampai menampilkan angka saat menekan tombol pada kalkulator

---
+++ script.js
@@ -0,0 +1,6 @@
+const numbers = document.querySelectorAll(".number")
+numbers.forEach((number)=>{
+    number.addEventListener("click", (event) =>{
+        console.log( event.target.value)
+    })
+})
